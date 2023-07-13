be6f6e6ab0763278bf926417ce55e86d10c47f73
stuckwithprogression <chukwunwemichael@gmail.com>
2023-07-13 09:44:24 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Well this a program that prints a linewith puts function just as instructed
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
