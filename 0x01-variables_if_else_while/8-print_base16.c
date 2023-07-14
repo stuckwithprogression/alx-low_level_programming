744758a66283310e631924a35eeb2480042c015f
stuckwithprogression <chukwunwemichael@gmail.com>
2023-07-14 20:24:04 +0000

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 28; i < 38; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (i = 99; i < 105; i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
