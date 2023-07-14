6ac1a87229f4b61e15323a4211fc3340ad076004
stuckwithprogression <chukwunwemichael@gmail.com>
2023-07-14 20:37:16 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 38; n < 48; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 39; m < 48; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (l = 40; l < 48; i++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 45 || m != 46)
+					if (n != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
