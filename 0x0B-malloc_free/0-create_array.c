b861ba04070e159bed00cdec7fa639b01542de82
stuckwithprogression <chukwunwemichael@gmail.com>
2023-08-08 20:53:06 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
+ * create_array - a function that creates an array of chars
+ *		  and initializes it with a specific char.
+ * @size: the size of an array
+ * @c: the character to assign
  * Description: create array of size size and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *str;
