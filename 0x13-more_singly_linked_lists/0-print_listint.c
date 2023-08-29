2e12a6f5daa702735f4c944af33f6b9394c503e2
stuckwithprogression <chukwunwemichael@gmail.com>
2023-08-29 15:19:33 -0700

    Correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t dig = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
+		dig++;
 		h = h->next;
 	}
 
-	return (num);
+	return (dig);
 }
