d2391cc075d6a10862396d05c45d0bc6b3299302
stuckwithprogression <chukwunwemichael@gmail.com>
2023-08-29 15:24:04 -0700

    Debug

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - Prints all the elements of listint_t list.
+ * @h: Pointer to list.
+ *
+ * Return: Number of nodes.
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t dig = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		dig++;
+		count++;
 		h = h->next;
 	}
-
-	return (dig);
+	return (count);
 }
