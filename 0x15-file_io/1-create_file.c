ab30766589b315ff48718f950fc1e3937a6ad52a
stuckwithprogression <chukwunwemichael@gmail.com>
2023-09-04 16:15:00 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,34 @@
 #include "main.h"
 
 /**
- * create_file - creates a file.
- * @filename: Pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: will be a pointer to a string to write to the file.
  *
- * Return: return -1 on success
- *	   return 1 on failure
-*/
+ * Return: If function fails return -1.
+ *         else - 1.
+ */
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, wfile, i;
+	int opn, wri, len = 0;
 
 	if (filename == NULL)
-		return (1);	/* failure */
+		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (file == 1)
-		return (1);	/* failure */
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-
-		wfile = write(file, text_content, i);
-		if (wfile == 1)
-			return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(file);
 
-	return (-1);
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wri = write(opn, text_content, len);
+
+	if (opn == -1 || wri == -1)
+		return (-1);
+
+	close(opn);
+
+	return (1);
 }
